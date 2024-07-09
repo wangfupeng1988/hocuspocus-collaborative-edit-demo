fe247d5958e0cfe7ed68f660a0da798f52a9099f
王福朋 <wangfupeng1988@163.com>
2024-07-09 16:33:19 +0800

    tiptap collab demo

--- hocuspocus-client-nextjs/src/app/demo3/page.tsx
+++ hocuspocus-client-nextjs/src/app/demo3/page.tsx
@@ -2,33 +2,29 @@
 
 import './styles.css'
 
-import { TiptapCollabProvider } from '@hocuspocus/provider'
+import { HocuspocusProvider } from '@hocuspocus/provider'
 import * as Y from 'yjs'
 
 import Editor from './Editor'
 
-const appId = process.env.NEXT_PUBLIC_TIPTAP_APP_ID || ''
-const token = process.env.NEXT_PUBLIC_TIPTAP_TOKEN
 const room = `room.${new Date().getFullYear().toString().slice(-2)}${
   new Date().getMonth() + 1
 }${new Date().getDate()}`
 
 // ydoc and provider for Editor A
 const ydocA = new Y.Doc()
-const providerA = new TiptapCollabProvider({
-  appId,
+const providerA = new HocuspocusProvider({
+  url: 'ws://127.0.0.1:1234',
   name: room,
   document: ydocA,
-  token,
 })
 
 // ydoc and provider for Editor B
 const ydocB = new Y.Doc()
-const providerB = new TiptapCollabProvider({
-  appId,
+const providerB = new HocuspocusProvider({
+  url: 'ws://127.0.0.1:1234',
   name: room,
   document: ydocB,
-  token,
 })
 
 const App = () => {
